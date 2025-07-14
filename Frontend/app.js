d0955ed96161de24e7233e802f4446e004582696
user.name like : "bihargamer10@gmail.com <shubhamgamer10@gmail.com>
2025-07-14 20:34:54 +0530

    first commit

---
+++ Frontend/app.js
@@ -0,0 +1,248 @@
+
+//  Apps Data 
+const apps = [
+      {
+        id: "spidyRwa",
+        title: "Spidy Rwa Apk",
+        description: "Advanced messaging app with privacy features",
+        image: "https://cdnx.jumpseller.com/yetiplantillas/image/40500692/resize/1200/1200?1696533142",
+        version:"v1.2",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/v1.0/SDV.RWA.apk"
+      },
+      {
+        id: "Kgs",
+        title: "Khan Global study",
+        description: "Optimize your phone for gaming performance",
+        image: "https://khanglobalstudies.com/Images/logos/logo.avif",
+        downloadUrl:""
+      },
+      {
+        id: "vikramjeet",
+        title: "RG vikramjeet pro",
+        description: "Stream and download your favorite movies",
+        image: "https://play-lh.googleusercontent.com/BOFZ0bsP6EdXjix_VrSNoYl1L-ax5_3vSgtqfgscbIUDD_3qEuiDfzOIOslZJK5uPkk",
+        downloadUrl:""
+      },
+      {
+        id: "NextTopper",
+        title: "Next Topper pro",
+        description: "Stream and download your favorite movies",
+        image: "https://d2alr3mt323vql.cloudfront.net/0/admin_v1/application_management/clientlogo/7521371540_next_topper_logo.png",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.1/NextTopper.apk"
+      },
+      {
+        id: "Billa Tv",
+        title: "Billa Tv pro",
+        description: "Stream and download your favorite movies",
+        image: "https://tse1.mm.bing.net/th/id/OIP.XAAIclKASeL302vldC-QUwHaHa?pid=Api&P=0&h=180",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.09/BILLATV.Mod.apk"
+      },
+      {
+        id: "SingaporeVpn",
+        title: "Singapore Vpn pro",
+        description: "Stream and download your favorite movies",
+        image: "https://play-lh.googleusercontent.com/1yzhGCgLZHGE3b15nXLon83IsyxfFRDHMaZqRkh47BzaOGbNUJVWZLZQpkd5LBS18eg",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.08/Singapore_VPN.5.apk"
+      },
+      {
+        id: "Hypic",
+        title: "Hypic pro",
+        description: "Stream and download your favorite movies",
+        image: "https://static-sg.winudf.com/wupload/xy/aprojectadmin/VUzf3tuJ.png",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.07/HypicProlatest.apk"
+      },  
+      {
+        id: "Capcut",
+        title: "Capcut pro",
+        description: "Stream and download your favorite movies",
+        image: "https://vectorseek.com/wp-content/uploads/2023/07/png-capcut-logo.png",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.06/CapCut.PRO.Updated.apk"
+      }, 
+      {
+        id: "Crickfy",
+        title: "Crickfy Tv pro",
+        description: "Stream and download your favorite movies",
+        image: "https://miro.medium.com/v2/resize:fit:512/1*XcYb7lON7DmhyIcWl58-zQ.png",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.05/CRICFyTvPro-.apk"
+      },
+      {
+        id: "Tempmail",
+        title: "Unlimited Gamil with OTP",
+        description: "Stream and download your favorite movies",
+        image: "https://tse1.mm.bing.net/th/id/OIP.gkM2D3eMRjvJW-MmveRUEwAAAA?pid=Api&P=0&h=180",
+        downloadUrl:"https://github.com/kepcoder/Apps/releases/download/V1.04/Temp.Mail.apk"
+      },                    
+];
+
+
+
+// Courses Data 
+const courses = [
+  {
+    id: "blender",
+    title: "Blender course",
+    description: "Learn to build responsive websites from scratch.",
+    image: "./public/images/blender.jpg",
+    link:'https://drive.google.com/drive/folders/1ff1KjCFJTVN5RfmNTERtC3H2w5g-lEuF'
+  },
+  {
+    id: "dropshipping",
+    title: "dropshipping",
+    description: "Master JS with real-world projects and challenges.",
+    image: "./images/dropshipping.jpg",
+    link:"https://drive.google.com/drive/folders/1Dkf0egkKsBeVPf5JbyOJ-3XCSJC1Boqk"
+  },
+  {
+    id: "englishSpoken",
+    title: "EnglishSpoken",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/englishspoken.jpg",
+    link:"https://drive.google.com/drive/folders/1ieeD6noUQ7E1TWsC9FADGzr8E4CGQzEH"
+  },
+  {
+    id: "kgsPocketGk",
+    title: "kgs Pocket GK",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/kgspocketgk.jpg",
+    link:"https://t.me/KHAN_POCKET_GK_OFFICIAL"
+  },
+  {
+    id: "Metaads",
+    title: "Meta Ads Full Course",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/metaAds_course.jpg",
+    link:"https://drive.google.com/drive/folders/1qjhDz90mJX1-9atUorIUq6AGZA_qHcyI?usp=sharing"
+  },
+  {
+    id: "Photoshop",
+    title: "Photosop 2025",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/photoshop_course.jpg",
+    link:"https://drive.google.com/drive/folders/1BoEzF7v7J5IsMhGRtzdf4_07T7ERzCC1"
+  },  
+  {
+    id: "Automation",
+    title: "Youtube Automation",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/youtube_automation.jpg",
+    link:"https://drive.google.com/drive/folders/1JlaiPnAG3S8__2-npi5ZMm-cqwehf7V1"
+  },
+  {
+    id: "flirting",
+    title: "Flirting Like pro",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/flirt.jpg",
+    link:"https://drive.google.com/drive/folders/1IeTEoCzugYV2ltCsBJ1K7w-yZ8TPw2rT?usp=sharing"
+  },
+  {
+    id: "attractmoney",
+    title: "Attract Money | Wealth Mastery",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/attractMoney.jpg",
+    link:"https://drive.google.com/drive/folders/1S0s3uMuzGKS6ZDqbdUmZYBke3OFFVoCb?usp=sharing"
+  },
+  {
+    id: "D2c",
+    title: "D2c with Arjun",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/D2c.jpg",
+    link:"https://drive.google.com/drive/folders/16bRcT-l-FYY5AiKszN16_to7ACyRqVqy?usp=sharing"
+  },  
+  {
+    id: "videoEditing",
+    title: "photos,Vsdco,Snapseed, Lightroom Course",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/videoediting.jpg",
+    link:"https://drive.google.com/drive/folders/1yawEe0m8pVL7VXqNjL8B6mwLqNt9AThL"
+  },
+  {
+    id: "PcEditing",
+    title: "Professional Editing Course",
+    description: "Dive into building modern SPAs using React.js",
+    image: "./images/pcediting.jpg",
+    link:"https://drive.google.com/drive/folders/1NPdzxDIiBlY4X2JV7KIexurAUYaUo0JR"
+  },    
+  {
+    id: "cw",
+    title: "Career Will All Courses",
+    image: "https://play-lh.googleusercontent.com/HZI08plsnKm1neoF_BN9AKYZFmHN8rUJNLA_ZA91JwJzASMTAe1fpGf8EA82NULhka8=h500",
+    link:"https://t.me/HtmlBatchess"
+  },    
+  {
+    id: "arjuna",
+    title: "ARJUNA JEE 2.0",
+    image: "https://d2bps9p1kiy4ka.cloudfront.net/5eb393ee95fab7468a79d189/c10f618c-b91d-460a-a82e-44d09b125195.jpg",
+    link:"https://t.me/Arjuna_2_0_Jee_Pw_lectures_2026"
+  },   
+];
+
+
+const coursesSection = document.getElementById("courses");
+if (coursesSection) {
+  courses.forEach(course => {
+    const card = document.createElement("div");
+    card.className = "app-card";
+    card.setAttribute("data-gsap", "");
+    card.innerHTML = `
+      <img src="${course.image}" alt="${course.title}">
+      <h3>${course.title}</h3>
+      <a class="download-btn" href=${course.link}>Claim Now</a>
+    `;
+    coursesSection.appendChild(card);
+  });
+}
+
+
+    const appsSection = document.getElementById("apps");
+    if (appsSection){
+    apps.forEach(app => {
+      const card = document.createElement("div");
+      card.className = "app-card";
+      card.setAttribute("data-gsap", "");
+      card.innerHTML = `
+        <img src="${app.image}" alt="${app.title}">
+        <h3>${app.title}</h3>
+        <p>${app.description}</p>
+        <a class="download-btn" href="apps.html?app=${app.id}">Download</a>
+      `;
+      appsSection.appendChild(card);
+    });
+    }
+
+
+
+
+
+
+
+
+    gsap.from("[data-gsap]", {
+      opacity: 0,
+      y: 30,
+      duration: 0.8,
+      stagger: 0.2,
+      ease: "power3.out"
+    });
+
+    const toggleBtn = document.getElementById("menu-toggle");
+    const nav = document.getElementById("navbar");
+    toggleBtn.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+
+    const popup = document.getElementById("popup-ad");
+    if(popup){
+        window.addEventListener("load", () => {
+          popup.style.display = "flex";
+          setTimeout(() => {
+            popup.style.display = "none";
+          }, 1000);
+        });
+
+    }
+
+
+
+
+
+
