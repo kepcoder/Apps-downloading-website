34c62537e49b078579d9baca8bd41bffe39dd505
user.name like : "bihargamer10@gmail.com <shubhamgamer10@gmail.com>
2025-07-15 14:56:22 +0530

    some canges

--- Frontend/app.js
+++ Frontend/app.js
@@ -231,7 +231,7 @@ if (coursesSection) {
           popup.style.display = "flex";
           setTimeout(() => {
             popup.style.display = "none";
-          }, 15000);
+          }, 10000);
         });
 
     }
